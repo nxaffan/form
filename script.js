b21e8d89dbfbf543fd95b19afe70057b83d56852
nxaffan <nxaffan95@gmail.com>
2026-03-10 06:19:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-document.getElementById("form").addEventListener("submit", function(e){
+ document.getElementById("form").addEventListener("submit", function(e){
 
 e.preventDefault();
 
 alert("Account Created Successfully!");
+
+});
+
+document.getElementById("mode-toggle").addEventListener("click", function(){
+
+const body = document.body;
+
+const toggle = this;
+
+if(body.classList.contains("night-mode")){
+
+body.classList.remove("night-mode");
+
+body.classList.add("day-mode");
+
+toggle.textContent = "🌙";
+
+}else{
+
+body.classList.remove("day-mode");
+
+body.classList.add("night-mode");
+
+toggle.textContent = "☀️";
+
+}
 
 });
